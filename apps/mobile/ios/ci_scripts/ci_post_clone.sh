441dedb6f162197b69d74fac0cd1020ea4846bdc
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-06-09 04:17:23 -0400

    feat: removing pnpm installation from CI post-clone script

--- apps/mobile/ios/ci_scripts/ci_post_clone.sh
+++ apps/mobile/ios/ci_scripts/ci_post_clone.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
 
-# Install pnpm
-npm install -g pnpm
+echo "Running Xcode Cloud post-clone"
 
-# Install JS dependencies from repo root
-cd "$CI_PRIMARY_REPOSITORY_PATH"
-pnpm install --frozen-lockfile
-
-# Install CocoaPods dependencies
 cd "$CI_PRIMARY_REPOSITORY_PATH/apps/mobile/ios"
+
 pod install --repo-update
