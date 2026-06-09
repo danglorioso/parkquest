8dd3317993f1cfdf6ae11fc872affe6243f45951
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-06-09 04:14:03 -0400

    feat: add CI post-clone script in iOS directory

---
+++ apps/mobile/ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+# Install pnpm
+npm install -g pnpm
+
+# Install JS dependencies from repo root
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+pnpm install --frozen-lockfile
+
+# Install CocoaPods dependencies
+cd "$CI_PRIMARY_REPOSITORY_PATH/apps/mobile/ios"
+pod install --repo-update
